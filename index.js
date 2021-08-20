a267f1efc56da5ad318ecc1a8684c82d751bfb6f
Vishal Prakash <v.r.prakash73@gmail.com>
2021-08-20 18:52:41 +1200

    adding projects endpoint for project filter to receive

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors);
 
 const db = mysql.createConnection({
-    host: "",
-    user: "",
-    password: "",
-    database: "",
+    host: "database-1.cznv306sxvoj.ap-southeast-2.rds.amazonaws.com",
+    user: "admin",
+    password: "admin123",
+    database: "missionx"
 });
 
+db.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Database connection successful !");
+    }
+});
+
+
+app.get('/projects', (req, res) => {
+    db.query('SELECT * FROM project', function (err, result) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+});
 
 app.listen(4000);
